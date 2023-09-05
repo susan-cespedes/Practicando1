78f8f1c44746a214271dda9e4140b6246d74867e
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-09-04 23:31:16 -0400

    TDD del 3ero

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,4 +1,4 @@
-import validarcampo from "./sumador.js";
+import {validarcampo,validarlongitud} from "./sumador.js";
 
 describe("VALIDAR CAMPO", () => {
   it("El post deberia tener un titulo", () => {
@@ -9,5 +9,10 @@ describe("VALIDAR CAMPO", () => {
   it("El post deberia tener un titulo lleno no vacio", () => {
     const dato=""
     expect(validarcampo(dato)).toEqual(false);
+  });
+
+  it("Titulo deberia ser maximo de 50 caracteres", () => {
+    const dato="holiiiiiiiiiiiiiiiiiiiiii"
+    expect(validarlongitud(dato)).toEqual(true);
   });
 });
